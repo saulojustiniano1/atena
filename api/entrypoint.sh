2dacdc4689d589cb2888469ecd6250f2426953c8
Saulo Justiniano <saulojustiniano.dev@gmail.com>
2025-09-28 20:37:31 -0300

    fix: docker | alteração no script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+wait_for_db() {
+    echo "Aguardando banco de dados..."
+    while ! nc -z db 5432; do
+        sleep 1
+    done
+    echo "Banco de dados está disponível!"
+}
+
+wait_for_db
+
 echo "Rodando migrations..."
 python manage.py migrate --noinput
 
